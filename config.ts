70045592371256b91dc90685a36543424ad83569
Raisan <ytpc26ms@outlook.jp>
2022-06-27 21:03:24 +0900

    Delete config.ts

--- config.ts
+++
@@ -1,9 +0,0 @@
-module.exports.config = {
-    // Discord API Token
-    DISCORD_API_TOKEN: "Nzc0OTMwNjk1OTczNjM0MDg4.GI89ZY.RhhgrDMjqE6aFoLHSgeJuNt0QhoQ_JpvhTHEy4",
-
-    // Debug
-    // Debug discord server's ID
-    // Default: undefined
-    DEBUG: undefined
-}
